b0234862c28b8f8186d443c9d846b81595513227
Douglas Padilha <douglastmpadilha@gmail.com>
2019-12-23 21:05:15 -0300

    Search input
    Addition of a search input to search order by clients

--- src/components/Orders.js
+++ src/components/Orders.js
@@ -13,9 +13,23 @@ const Orders = () => {
         })
     }, [])
 
+    const searchHandle = (event) => {
+        Axios.get('/api/orders?search='+ event.target.value)
+        .then(response => {
+            const ordersData = ([...response.data])
+            setOrders(ordersData)
+        })
+    }
+
 
     return (
-        <div>
+        <div className="container mt-5">
+            <div className="container h-100">
+                <div className="d-flex justify-content-center h-100">
+                    <input className="search_input" type="text" name="" onChange={searchHandle} placeholder="Digite o nome do cliente"/>
+                    <a href="/#" className="search_icon"><i className="fas fa-search"></i></a>
+                </div>
+            </div>
             {useMemo(() => orders ? <OrderCard registers={orders}/> : <div></div>, [orders])}
         </div>
     )
